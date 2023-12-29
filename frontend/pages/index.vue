39f9da62ab871a2b651970bffc2d992892968f6d
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2023-12-30 07:50:09 +0900

    update index.vue

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,7 +2,7 @@
     <div class="py-40 px-6 bg-[#1a1a1a] text-center">
         <h1 class="text-5xl text-white">My portfolio site</h1>
     </div>
-    <div class="grid md:grid-cols-4 gap-3 py-10 px-6 text-white">
+    <div class="grid md:grid-cols-4 gap-6 py-10 px-6 text-white">
         <main class="md:col-span-3">
             <ul class="mb-8">
                 <li class="flex flex-col mb-8 w-full p-5 bg-[#3a3a3a] md:flex-row" key="">
@@ -43,12 +43,13 @@
                 </li>
             </ul>
         </main>
-        <aside class="md:col-span-1 md:pl-4">
+        <aside class="md:col-span-1">
             <div class="p-5 bg-[#3a3a3a]">
                 <h2 class="text-2xl mb-4 border-b-2 border-[#fff]">Profile</h2>
                 <div class="mb-20">
                     <div class="py-8">
-                        <img src="/pongki2.jpg" alt="" class="rounded-full max-w-[150px] w-[100%] max-h-[150px] h-[100%] m-auto mb-6" />
+                        <img src="/pongki2.jpg" alt=""
+                            class="rounded-full max-w-[150px] w-[100%] max-h-[150px] h-[100%] m-auto mb-6" />
                     </div>
                     <p class="text-center">S-Ohtani</p>
                     <p class="text-center">Passionate developer</p>
