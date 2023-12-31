6933dbba62a2dfd137bc289170dd4d34633b07d9
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2023-12-31 17:40:17 +0900

    update config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      style: [{ children: "body { background: #0f0f0f; }" }],
+    },
+  },
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   postcss: {
