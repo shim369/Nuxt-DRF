c57963ccb0957360c06b23e58a83d634b87a7ede
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2023-12-31 17:40:36 +0900

    add file

---
+++ frontend/pages/admin.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="py-10 px-6">
+        <h1 class="mb-6 text-2xl">Admin</h1>
+
+        <ul class="space-y-4">
+            <project :my="true" />
+            <project :my="true" />
+            <project :my="true" />
+        </ul>
+    </div>
+</template>
