92cffb3c874a91cc6a75fe1bcbabb8fdf1bc3da8
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-01-01 19:34:31 +0900

    update views

--- backend/portfolio_app/views.py
+++ backend/portfolio_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import status, authentication, permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Project, Skill
+from .serializers import ProjectSerializer
+
+class NewestProjectsView(APIView):
+    def get(self, request, format=None):
+        projects = Project.objects.all()[0:4]
+        serializer = ProjectSerializer(projects, many=True)
+
+        return Response(serializer.data)
