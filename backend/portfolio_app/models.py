27655c1aa978533d0d2923f79fa2bca8b5b58ba8
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-01-01 19:33:51 +0900

    update models

--- backend/portfolio_app/models.py
+++ backend/portfolio_app/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.template import defaultfilters
 
-# Create your models here.
+class Skill(models.Model):
+    title = models.CharField('スキル', max_length=255)
+
+    class Meta:
+        ordering = ('title',)
+
+class Project(models.Model):
+    skill = models.ForeignKey(Skill, related_name="projects", on_delete=models.CASCADE)
+    title = models.CharField('タイトル', max_length=255)
+    description = models.CharField('説明', max_length=500)
+    demo_link = models.URLField('リンク', max_length=200)
+    github_repo = models.URLField('GitHub', max_length=200)
+    content = models.TextField('テキスト')
+    created_at = models.DateTimeField('作成日', auto_now_add=True)
+    created_by = models.ForeignKey(User, verbose_name='作者', related_name="projects", on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('-created_at',)
+
+    def created_at_formatted(self):
+        return defaultfilters.date(self.created_at, 'M d, Y')
