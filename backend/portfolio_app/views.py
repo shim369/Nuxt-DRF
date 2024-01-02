ba51b8bc78325aaa0145d269539971b9efdd7cf4
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-01-02 18:01:12 +0900

    update views

--- backend/portfolio_app/views.py
+++ backend/portfolio_app/views.py
@@ -3,11 +3,42 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .models import Project, Skill
-from .serializers import ProjectSerializer
+from .serializers import ProjectSerializer, ProjectDetailSerializer, SkillSerializer
 
+class SkillsView(APIView):
+    def get(self, request, format=None):
+        skills = Skill.objects.all()
+        serializer = SkillSerializer(skills, many=True)
+
+        return Response(serializer.data)
+    
 class NewestProjectsView(APIView):
     def get(self, request, format=None):
         projects = Project.objects.all()[0:4]
         serializer = ProjectSerializer(projects, many=True)
+
+        return Response(serializer.data)
+    
+class AllProjectsView(APIView):
+    def get(self, request, format=None):
+        projects = Project.objects.all()
+        skills = request.GET.get('skills', '')
+        query = request.GET.get('query', '')
+
+        if query:
+            projects = projects.filter(title__icontains=query)
+
+        if skills:
+            projects = projects.filter(skill_id__in=skills.split(','))
+
+
+        serializer = ProjectSerializer(projects, many=True)
+
+        return Response(serializer.data)
+
+class ProjectsDetailView(APIView):
+    def get(self, request, pk, format=None):
+        project = Project.objects.get(pk=pk)
+        serializer = ProjectDetailSerializer(project)
 
         return Response(serializer.data)
