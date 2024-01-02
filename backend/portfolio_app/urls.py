777e1dcb6430fcb0fb57a51d3a8c957f4076907c
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-01-02 18:00:55 +0900

    update urls

--- backend/portfolio_app/urls.py
+++ backend/portfolio_app/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.AllProjectsView.as_view()),
+    path('skills/', views.SkillsView.as_view()),
     path('newest/', views.NewestProjectsView.as_view()),
+    path('<int:pk>/', views.ProjectsDetailView.as_view()),
 ]
