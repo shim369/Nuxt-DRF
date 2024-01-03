1931d38f2fc97b4dd510fe0d0e0bea627206b2a9
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-01-03 13:39:01 +0900

    add login & signup function

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,11 +1,63 @@
+<script setup>
+import { useUserStore } from '@/store/user';
+const router = useRouter()
+const userStore = useUserStore()
+
+let email = ref('')
+let password = ref('')
+let errors = ref([])
+
+async function submitForm() {
+    console.log('submitForm')
+
+    errors.value = []
+
+    await $fetch('http://127.0.0.1:8000/api/v1/token/login/', {
+        method: 'POST',
+        body: {
+            username: email.value,
+            password: password.value,
+        }
+    })
+    .then(data => {
+        console.log('response', data.auth_token)
+
+        userStore.setToken(data.auth_token, email.value)
+
+        router.push({path: '/'})
+    })
+    .catch(error => {
+        if (error.response) {
+            for (const property in error.response._data) {
+                errors.value.push(`${property}: ${error.response._data[property]}`)
+            }
+            console.log(JSON.stringify(error.response))
+        } else if (error.message) {
+            errors.value.push('Something went wrong. Please try again')
+
+            console.log(JSON.stringify(error))
+        }
+    })
+}
+</script>
 <template>
     <div class="py-10 px-6">
         <div class="max-w-sm mx-auto py-10 px-6 bg-[#3a3a3a]">
             <h1 class="mb-6 text-2xl">Log in</h1>
 
-            <form>
-                <input type="email" placeholder="Email" class="w-full mb-4 py-4 px-6">
-                <input type="password" placeholder="Password" class="w-full mb-4 py-4 px-6">
+            <form v-on:submit.prevent="submitForm">
+                <input v-model="email" type="email" placeholder="Email" class="w-full mb-4 py-4 px-6 text-black">
+                <input v-model="password" type="password" placeholder="Password" class="w-full mb-4 py-4 px-6 text-black">
+                
+                <div
+                    v-if="errors.length"
+                    class="mb-6 py-4 px-6 bg-[#f84934] text-white"
+                >
+                    <p v-for="error in errors" v-bind:key="error">
+                        {{ error }}
+                    </p>
+                </div>
+                
                 <button class="py-4 px-6 text-white bg-[#E01A00]">Submit</button>
             </form>
         </div>
