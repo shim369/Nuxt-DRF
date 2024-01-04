2e50c0629a4f1a86c6474c4bc015f0404cb9374a
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-01-04 15:39:25 +0900

    changed the output method of Skills

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,5 +1,6 @@
 <script setup>
 const { data: projects } = await useFetch('http://127.0.0.1:8000/api/v1/projects/newest/')
+let { data: projectsSkills } = await useFetch('http://127.0.0.1:8000/api/v1/projects/skills/')
 
 // console.log(projects.value)
 
@@ -16,16 +17,12 @@ useSeoMeta({
     <div class="grid md:grid-cols-4 gap-6 py-10 px-6 text-white">
         <main class="md:col-span-3">
             <ul class="space-y-4">
-                <project
-                    v-for="project in projects"
-                    v-bind:key="project.id"
-                    v-bind:project="project"
-                />
+                <project v-for="project in projects" v-bind:key="project.id" v-bind:project="project" />
             </ul>
         </main>
         <aside class="md:col-span-1">
             <div class="p-5 bg-[#3a3a3a]">
-                <h2 class="text-2xl mb-4 border-b-2 border-[#000] pb-2">Profile</h2>
+                <h2 class="h2-title">Profile</h2>
                 <div class="mb-20">
                     <div class="py-8">
                         <img src="/pongki2.jpg" alt=""
@@ -35,10 +32,11 @@ useSeoMeta({
                     <p class="text-center">Passionate developer</p>
                 </div>
 
-                <h2 class="text-2xl mb-4 border-b-2 border-[#000] pb-2">Skills</h2>
+                <h2 class="h2-title">Skills</h2>
                 <div class="mb-8 flex flex-row break-all">
-                    <p class="mr-2">Python</p>
-                    <p>JavaScript</p>
+                    <p v-for="skill in projectsSkills" v-bind:key="skill.id" class="mr-2 text-white">
+                        {{ skill.title }}
+                    </p>
                 </div>
             </div>
         </aside>
