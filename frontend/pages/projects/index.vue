550ecc6df22c04b801c46882d6befef013324a5d
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-01-04 14:31:09 +0900

    update document format

--- frontend/pages/projects/index.vue
+++ frontend/pages/projects/index.vue
@@ -35,8 +35,7 @@ let { data: projects } = await useFetch('http://127.0.0.1:8000/api/v1/projects/'
                 <input v-model="query" type="search" placeholder="Find project" class="w-full px-6 py-4 text-black">
                 <button
                     class="px-6 py-6 text-white bg-[#E01A00] border-2 border-[#E01A00] hover:bg-transparent hover:text-[#E01A00] hover:border-2 hover:border-[#E01A00]"
-                    v-on:click="performSearch"
-                >
+                    v-on:click="performSearch">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round"
@@ -47,24 +46,15 @@ let { data: projects } = await useFetch('http://127.0.0.1:8000/api/v1/projects/'
             <hr class="my-6 opacity-30">
             <h3 class="mt-6 text-xl text-white">Skills</h3>
             <div class="mt-6 space-y-4">
-                <p
-                    v-for="skill in projectsSkills"
-                    v-bind:key="skill.id"
-                    v-on:click="toggleSkill(skill.id)"
-                    class="py-4 px-6 text-white"
-                    v-bind:class="{'bg-gray-500': selectedSkillsRef.includes(skill.id)}"
-                >
+                <p v-for="skill in projectsSkills" v-bind:key="skill.id" v-on:click="toggleSkill(skill.id)"
+                    class="py-4 px-6 text-white" v-bind:class="{ 'bg-gray-500': selectedSkillsRef.includes(skill.id) }">
                     {{ skill.title }}
                 </p>
             </div>
         </aside>
         <main class="md:col-span-3">
             <ul class="space-y-4">
-                <project
-                    v-for="project in projects"
-                    v-bind:key="project.id"
-                    v-bind:project="project"
-                />
+                <project v-for="project in projects" v-bind:key="project.id" v-bind:project="project" />
             </ul>
         </main>
     </div>
