4f976a000e24a73c359e43ba6f55d5233808f6bc
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-01-04 14:32:06 +0900

    update post project page

--- frontend/pages/edit/[id].vue
+++ frontend/pages/edit/[id].vue
@@ -18,6 +18,7 @@ let description = ref(project.value.description)
 let image_url = ref(project.value.image_url)
 let demo_link = ref(project.value.demo_link)
 let github_repo = ref(project.value.github_repo)
+let content = ref(project.value.content)
 let errors = ref([])
 
 
@@ -31,6 +32,7 @@ async function submitForm() {
     if (description.value == '') { errors.value.push('The description field is missing') }
     if (demo_link.value == '') { errors.value.push('The link field is missing') }
     if (github_repo.value == '') { errors.value.push('The github field is missing') }
+    if (content.value == '') { errors.value.push('The content field is missing') }
 
     if (errors.value.length == 0) {
         await $fetch('http://127.0.0.1:8000/api/v1/projects/' + route.params.id + '/edit/', {
@@ -45,25 +47,26 @@ async function submitForm() {
                 image_url: image_url.value,
                 demo_link: demo_link.value,
                 github_repo: github_repo.value,
+                content: content.value,
             }
         })
-            .then(response => {
-                console.log('response', response)
+        .then(response => {
+            console.log('response', response)
 
-                router.push({ path: '/admin' })
-            })
-            .catch(error => {
-                if (error.response) {
-                    for (const property in error.response._data) {
-                        errors.value.push(`${property}: ${error.response._data[property]}`)
-                    }
-                    console.log(JSON.stringify(error.response))
-                } else if (error.message) {
-                    errors.value.push('Something went wrong. Please try again')
-
-                    console.log(JSON.stringify(error))
+            router.push({ path: '/admin' })
+        })
+        .catch(error => {
+            if (error.response) {
+                for (const property in error.response._data) {
+                    errors.value.push(`${property}: ${error.response._data[property]}`)
                 }
-            })
+                console.log(JSON.stringify(error.response))
+            } else if (error.message) {
+                errors.value.push('Something went wrong. Please try again')
+
+                console.log(JSON.stringify(error))
+            }
+        })
     }
 }
 </script>
@@ -97,6 +100,11 @@ async function submitForm() {
             <div>
                 <label>GitHub</label>
                 <input v-model="github_repo" type="text" class="w-full mt-2 p-4 text-black">
+            </div>
+
+            <div>
+                <label>Content</label>
+                <textarea v-model="content" type="text" class="w-full mt-2 p-4 text-black"></textarea>
             </div>
 
             <div v-if="errors.length" class="mb-6 py-4 px-6 bg-[#f84934] text-white">
